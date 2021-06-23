1ebf2a3848e9d84214b3697ccb89b1ec46ae7757
Adam Mann <aemann2@gmail.com>
2021-06-23 10:01:50 -0700

    initial commit

---
+++ app.js
@@ -0,0 +1,72 @@
+const express = require('express');
+const app = express();
+const ejs = require('ejs');
+const mongoose = require('mongoose');
+const content = require('./exports/content');
+const dotenv = require('dotenv');
+const connectDB = require('./config/db');
+
+dotenv.config({path: './config/config.env'});
+
+app.use(
+  express.urlencoded({
+    extended: true,
+  })
+);
+
+app.set('view engine', 'ejs');
+app.use(express.static('public'));
+
+connectDB();
+
+const blogSchema = new mongoose.Schema({
+  title: String,
+  content: String
+})
+
+const Post = mongoose.model('Post', blogSchema);
+
+app.get('/', (req, res) => {
+  Post.find({}, (err, items) => {
+    res.render('home', {
+      startingContent: content.homeStartingContent,
+      posts: items,
+    });
+  })
+});
+
+app.get('/post/:id', (req, res) => {
+  const post = Post.findById(req.params.id, (err, post) => {
+    if (post) {
+      res.render('post', { title: post.title, content: post.content });
+    } else {
+      res.render('sorry');
+    }
+  });
+});
+
+app.get('/about', (req, res) => {
+  res.render('about', { aboutContent: content.aboutContent });
+});
+
+app.get('/contact', (req, res) => {
+  res.render('contact', { contactContent: content.contactContent });
+});
+
+app.get('/compose', (req, res) => {
+  res.render('compose');
+});
+
+app.post('/compose', (req, res) => {
+  Post.create({
+    title: req.body.postTitle,
+    content: req.body.postContent
+  })
+
+  // redirect to the home page for the response
+  res.redirect('/');
+});
+
+app.listen(3000, function () {
+  console.log('Server started on port 3000');
+});
